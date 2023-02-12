7b6a0c69a9a7551c1419481f9e8bdd934828a3b3
ta9yamakawa <myttari1206@gmail.com>
2023-02-12 21:14:48 +0900

    create project

---
+++ MolkkyScoreBoard/MolkkyScoreBoard/MolkkyScoreBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MolkkyScoreBoardApp.swift
+//  MolkkyScoreBoard
+//
+//  Created by ta9yamakawa on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct MolkkyScoreBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
