00ab2ec11e99e51c6d63355cd464be4533e344e7
ta9yamakawa <myttari1206@gmail.com>
2023-02-15 03:20:49 +0900

    ScrollViewを除く

--- MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/StartMenu/StartMenuView.swift
+++ MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/StartMenu/StartMenuView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 /// プレイスタート画面
 struct StartMenuView: View {
 
+    /// 最小チーム数
+    private static let minTeamCount = 1
+
+    /// 最大チーム数
+    private static let maxTeamCount = 6
+
     /// GridItem
     private static let gridItem = GridItem(.flexible(minimum: 140, maximum: 180))
 
@@ -18,27 +24,26 @@ struct StartMenuView: View {
 
     var body: some View {
         VStack(spacing: 30) {
-            Text("何チームでプレイしますか？")
+            Text("何チームでモルックを\nプレイしますか？")
                 .font(Font.system(size: 20))
+                .multilineTextAlignment(.center)
                 .padding(.top, 40)
-            ScrollView {
-                LazyVGrid(columns: columns, spacing: 10) {
-                    ForEach((1...6), id: \.self) { index in
-                        Button(action: {
-                            print("tapped")
-                        }, label: {
-                            Text("\(index)チーム")
-                                .font(Font.system(size: 20))
-                                .foregroundColor(.white)
-                        })
-                        .frame(width: 120)
-                        .padding(.vertical, 12)
-                        .background(Color.orange)
-                        .cornerRadius(4)
-                    }
+
+            LazyVGrid(columns: columns, spacing: 10) {
+                ForEach((type(of: self).minTeamCount...type(of: self).maxTeamCount), id: \.self) { index in
+                    Button(action: {
+                        print("tapped")
+                    }, label: {
+                        Text("\(index)チーム")
+                            .font(Font.system(size: 20))
+                            .foregroundColor(.white)
+                    })
+                    .frame(width: 140)
+                    .padding(.vertical, 12)
+                    .background(Color.orange)
+                    .cornerRadius(4)
                 }
             }
-            .padding(.horizontal, 10.0)
 
             Spacer()
         }
@@ -50,11 +55,11 @@ struct StartMenuView_Previews: PreviewProvider {
 
         ForEach(["iPhone SE (3rd generation)",
                  "iPad Pro (12.9-inch) (6th generation)",
-                "iPhone 14"],
+                 "iPhone 14"],
                 id: \.self) { deviceName in
             StartMenuView()
-                        .previewDevice(PreviewDevice(rawValue: deviceName))
-                        .previewDisplayName(deviceName)
-                }
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
