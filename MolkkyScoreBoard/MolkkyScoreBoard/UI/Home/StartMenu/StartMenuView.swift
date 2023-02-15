3c88adef267a72bc0421e904b38794db925e3bba
ta9yamakawa <myttari1206@gmail.com>
2023-02-16 00:56:16 +0900

    ボタンを切り分ける

--- MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/StartMenu/StartMenuView.swift
+++ MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/StartMenu/StartMenuView.swift
@@ -11,10 +11,10 @@ import SwiftUI
 struct StartMenuView: View {
 
     /// 最小チーム数
-    private static let minTeamCount = 1
+    private let minTeamCount = 1
 
     /// 最大チーム数
-    private static let maxTeamCount = 6
+    private let maxTeamCount = 6
 
     /// GridItem
     private static let gridItem = GridItem(.flexible(minimum: 140, maximum: 180))
@@ -30,18 +30,9 @@ struct StartMenuView: View {
                 .padding(.top, 40)
 
             LazyVGrid(columns: columns, spacing: 10) {
-                ForEach((type(of: self).minTeamCount...type(of: self).maxTeamCount), id: \.self) { index in
-                    Button(action: {
-                        print("tapped")
-                    }, label: {
-                        Text("\(index)チーム")
-                            .font(Font.system(size: 20))
-                            .foregroundColor(.white)
-                    })
-                    .frame(width: 140)
-                    .padding(.vertical, 12)
-                    .background(Color.orange)
-                    .cornerRadius(4)
+                ForEach((minTeamCount...maxTeamCount),
+                        id: \.self) { teamCount in
+                    TeamSelectButtonView(teamCount: teamCount)
                 }
             }
 
@@ -50,6 +41,7 @@ struct StartMenuView: View {
     }
 }
 
+// MARK: Previews
 struct StartMenuView_Previews: PreviewProvider {
     static var previews: some View {
 
