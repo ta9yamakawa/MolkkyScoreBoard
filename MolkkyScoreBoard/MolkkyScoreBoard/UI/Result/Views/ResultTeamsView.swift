2c2fb1bf8dfe4a3f7eb3b43139df81433a26136e
ta9yamakawa <myttari1206@gmail.com>
2023-03-22 09:48:00 +0900

    結果表示用のView作成

---
+++ MolkkyScoreBoard/MolkkyScoreBoard/UI/Result/Views/ResultTeamsView.swift
@@ -0,0 +1,38 @@
+//
+//  ResultTeamsView.swift
+//  MolkkyScoreBoard
+//
+//  Created by ta9yamakawa on 2023/03/22.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+/// チームの結果 View
+struct ResultTeamsView: View {
+
+    /// View Store
+    let viewStore: ViewStoreOf<ResultFeature>
+
+    var body: some View {
+        List {
+            ForEach(viewStore.state.teams) { team in
+                // TODO: 結果表示用のViewに変更する
+                OrderingTeamView(team: team)
+                    .listRowInsets(EdgeInsets(top: .zero,
+                                              leading: .zero,
+                                              bottom: 10,
+                                              trailing: 10))
+            }
+        }
+    }
+}
+
+struct ResultTeamsView_Previews: PreviewProvider {
+    static var previews: some View {
+        let state = ResultFeature.State(teams: TeamsMock().data)
+        let viewStore = ViewStore(StoreOf<ResultFeature>(initialState: state,
+                                                         reducer: ResultFeature()))
+        ResultTeamsView(viewStore: viewStore)
+    }
+}
