c7c876c03f9485420cb4c1625f15acdef8663e89
ta9yamakawa <myttari1206@gmail.com>
2023-04-22 19:05:45 +0900

    routerの持たせ方を修正

--- MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/HomeTabView.swift
+++ MolkkyScoreBoard/MolkkyScoreBoard/UI/Home/HomeTabView.swift
@@ -20,9 +20,6 @@ struct HomeTabView: View {
     /// 選択中のタブ
     @State private var selectedTab: TabType = .play
 
-    /// Router
-    @StateObject private var router = PageRouter.shared
-
     /// Initialize
     init() {
         NavigationViewAppearance.configure()
@@ -30,7 +27,7 @@ struct HomeTabView: View {
     }
 
     var body: some View {
-        DestinationHolderView(router: router) {
+        DestinationHolderView() {
             TabView(selection: $selectedTab) {
                 StartMenuView()
                     .tabItem {
