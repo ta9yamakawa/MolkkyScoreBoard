2bc7b895cb20b1f680cbf27c2e8f51f1cba350e5
ta9yamakawa <myttari1206@gmail.com>
2023-05-09 01:23:11 +0900

    カラー定義

--- MolkkyScoreBoard/MolkkyScoreBoard/Utils/AppColor.swift
+++ MolkkyScoreBoard/MolkkyScoreBoard/Utils/AppColor.swift
@@ -10,26 +10,56 @@ import SwiftUI
 
 /// アプリ内で使うカラー定義
 enum AppColor {
-    /// アクセントカラー
+    // MARK: アクセント系カラー
+    /// 赤
     case accent
+    /// オレンジ
+    case accent2
+    /// 肌色
+    case accent3
+
     /// メインカラー
     case main
     /// ベースカラー
     case base
     /// エラーカラー
     case error
+    /// スキットルの色
+    case skittle
+    /// キャンセル系カラー
+    case cancel
+    /// フォントカラー
+    case font
+    /// 白色
+    case white
+    /// 薄めの青
+    case lightBlue
 
     /// カラーアセットの名前
     private var name: String {
         switch self {
         case .accent:
             return "accent"
+        case .accent2:
+            return "accent2"
+        case .accent3:
+            return "accent3"
         case .main:
             return "main"
         case .base:
             return "base"
         case .error:
             return "error"
+        case .skittle:
+            return "skittle"
+        case .cancel:
+            return "cancel"
+        case .font:
+            return "font"
+        case .white:
+            return "white"
+        case .lightBlue:
+            return "lightBlue"
         }
     }
 
